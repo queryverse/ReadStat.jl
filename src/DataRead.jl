d31c79c1bdaacd1f3b8db8d5b675845372cea353
matthieugomez <gomez.matthieu@gmail.com>
2015-10-12 18:43:04 -0400

    correct issue

--- src/DataRead.jl
+++ src/DataRead.jl
@@ -1,10 +1,9 @@
 module DataRead
-
-using DataArrays
-import DataFrames: DataFrame
+using NullableArrays
+using DataStreams
+using DataFrames
 
 export read_dta, read_sav, read_por, read_sas7bdat
-
 
 ##############################################################################
 ##
@@ -12,25 +11,25 @@ export read_dta, read_sav, read_por, read_sas7bdat
 ##
 ##############################################################################
 
-const READSTAT_TYPE_STRING      = 0
-const READSTAT_TYPE_CHAR        = 1
-const READSTAT_TYPE_INT16       = 2
-const READSTAT_TYPE_INT32       = 3
-const READSTAT_TYPE_FLOAT       = 4
-const READSTAT_TYPE_DOUBLE      = 5
-const READSTAT_TYPE_LONG_STRING = 6
+const READSTAT_TYPE_STRING      = Cint(0)
+const READSTAT_TYPE_CHAR        = Cint(1)
+const READSTAT_TYPE_INT16       = Cint(2)
+const READSTAT_TYPE_INT32       = Cint(3)
+const READSTAT_TYPE_FLOAT       = Cint(4)
+const READSTAT_TYPE_DOUBLE      = Cint(5)
+const READSTAT_TYPE_LONG_STRING = Cint(6)
 
-const READSTAT_ERROR_OPEN       = 1
-const READSTAT_ERROR_READ       = 2
-const READSTAT_ERROR_MALLOC     = 3
-const READSTAT_ERROR_USER_ABORT = 4
-const READSTAT_ERROR_PARSE      = 5
+const READSTAT_ERROR_OPEN       = Cint(1)
+const READSTAT_ERROR_READ       = Cint(2)
+const READSTAT_ERROR_MALLOC     = Cint(3)
+const READSTAT_ERROR_USER_ABORT = Cint(4)
+const READSTAT_ERROR_PARSE      = Cint(5)
 
 immutable DataReadValue
-    v::Cdouble
+    union::Int64
     readstat_types_t::Cint
-    is_system_missing::Cint  
-    is_considered_missing::Cint       
+    tag::Cchar
+    bits::UInt8
 end
 
 function get_type(value::DataReadValue)
@@ -41,6 +40,8 @@ function get_ismissing(value::DataReadValue)
 end
 
 
+# Define DataReadVariable to dispatch on get_type.
+# Define the type while we're at it
 immutable DataReadMissingness
     missing_ranges::DataReadValue
     missing_ranges_count::Clong
@@ -65,26 +66,8 @@ function get_name(variable::Ptr{DataReadVariable})
 end
 
 function get_type(variable::Ptr{DataReadVariable})
-    ccall((:readstat_variable_get_type, "libreadstat"), Cint, (Ptr{DataReadVariable},), variable)
+    ccall((:readstat_variable_get_type, "libreadstat"), Cint, (Ptr{DataReadVariable},), variable)::Cint
 end
-
-
-##############################################################################
-##
-## Pure Julia type
-##
-## Note: ctx.colnames[index+1] is not type stable
-## Performance wise, use something like in https://github.com/quinnj/CSV.jl
-##
-##############################################################################
-
-type DataReadCtx
-    nrows::Int
-    colnames::Vector{Symbol}
-    columns::Vector{Any}
-end
-DataReadCtx() = DataReadCtx(0, Array(Symbol, (0,)), Array(Any, (0,)))
-DataFrame(ctx::DataReadCtx) = DataFrame(ctx.columns, ctx.colnames)
 
 ##############################################################################
 ##
@@ -92,27 +75,23 @@ DataFrame(ctx::DataReadCtx) = DataFrame(ctx.columns, ctx.colnames)
 ##
 ##############################################################################
 
-function handle_info!(obs_count::Cint, var_count::Cint, ctx_ptr::Ptr{DataReadCtx})
-    nrows = convert(Int, obs_count)
-    ncols = convert(Int, var_count)
-    colnames = Array(Symbol, (ncols,))
-    columns = Array(Any, (ncols,))
-
-    ctx = unsafe_pointer_to_objref(ctx_ptr)
-    ctx.nrows = nrows
-    ctx.colnames = colnames
-    ctx.columns = columns
-
+function handle_info!(obs_count::Cint, var_count::Cint, ds_ptr::Ptr{DataStream})
+    ds = unsafe_pointer_to_objref(ds_ptr)
+    ds.schema.rows = convert(Int, obs_count)
+    ds.schema.cols = convert(Int, var_count)
+    ds.schema.header = Array(UTF8String, var_count)
+    ds.schema.types = Array(DataType, var_count)
     return convert(Cint, 0)
 end
 
-function handle_variable!(index::Cint, variable::Ptr{DataReadVariable}, variable_label::Cstring,  ctx_ptr::Ptr{DataReadCtx})
+function handle_variable!(var_index::Cint, variable::Ptr{DataReadVariable}, 
+                        variable_label::Cstring,  ds_ptr::Ptr{DataStream})
+    col = var_index + 1
+    ds = unsafe_pointer_to_objref(ds_ptr)::DataStream
 
     name_str = get_name(variable)
     data_type = get_type(variable)
-
-    ctx = unsafe_pointer_to_objref(ctx_ptr)::DataReadCtx
-    ctx.colnames[index+1] = convert(Symbol, name_str)
+    ds.schema.header[col] = convert(UTF8String, name_str)
 
     jtype = Float64
     if data_type == READSTAT_TYPE_STRING
@@ -128,55 +107,57 @@ function handle_variable!(index::Cint, variable::Ptr{DataReadVariable}, variable
     elseif data_type == READSTAT_TYPE_DOUBLE
         jtype = Float64
     end
-    ctx.columns[index+1] = DataArray(jtype, (ctx.nrows,))
+    ds.schema.types[col] = jtype
+    push!(ds.data, NullableArray(jtype, ds.schema.rows))
     return convert(Cint, 0)
 end
 
-function handle_value!(obs_index::Cint, var_index::Cint, value::DataReadValue, ctx_ptr::Ptr{DataReadCtx})
- 
-    jvalue = NA
-    data_type = get_type(value)
-    vmissing = get_ismissing(value)
-    # starts to goes wrong here
-    @show data_type
-    @show vmissing
-    @show value
-    @show reinterpret(Float64, value.v)
+function handle_value!(obs_index::Cint, var_index::Cint, 
+                        value::DataReadValue, ds_ptr::Ptr{DataStream})
+    col = var_index + 1
+    row = obs_index + 1
+    ds = unsafe_pointer_to_objref(ds_ptr)::DataStream
 
-    # this produces segault
-    #  unsafe_pointer_to_objref(ctx_ptr)::DataReadCtx
-    if vmissing != 0
-        if data_type == READSTAT_TYPE_STRING
-            jvalue = reinterpret(Ptr{UInt8}, value.v)
-        elseif data_type == READSTAT_TYPE_CHAR
-            jvalue = reinterpret(UInt8, value.v)
-        elseif data_type == READSTAT_TYPE_INT16
-            jvalue = reinterpret(Int16, value.v)
-        elseif data_type == READSTAT_TYPE_INT32
-            jvalue = reinterpret(Int32, value.v)
-        elseif data_type == READSTAT_TYPE_FLOAT
-            jvalue = reinterpret(Float32, value.v)
-        elseif data_type == READSTAT_TYPE_DOUBLE
-            jvalue =  reinterpret(Float64, value.v)
-        end
+    vmissing = get_ismissing(value)
+    isnull = convert(Bool, vmissing)
+    # data_type = get_type(value) does not work
+    # so I use type of ds column
+    if isnull
+        readfield!(ds.data[col], ds.schema.types[col], row, col)
+    else
+        readfield!(ds.data[col], ds.schema.types[col], row, col, value.union)
     end
     return convert(Cint, 0)
 end
 
-function handle_value_label!(val_labels::Cstring, value::DataReadValue, label::Cstring, ctx_ptr::Ptr{DataReadCtx})
-    ctx = unsafe_pointer_to_objref(ctx_ptr)
+@inline function readfield!(dest::NullableVector{ASCIIString}, ::Type{ASCIIString}, row, col, val)
+    val = bytestring(reinterpret(Ptr{Int8}, val))
+    @inbounds dest.values[row], dest.isnull[row] = val, false
+end
+
+@inline function readfield!{T}(dest::NullableVector{T}, ::Type{T}, row, col, val)
+    # is it the faster way to reinterpret objects with potentially smaller size?
+    val = unsafe_load(convert(Ptr{T}, pointer_from_objref(val)))::T
+    @inbounds dest.values[row], dest.isnull[row] = val, false
+end
+
+function readfield!{T}(dest::NullableVector{T}, ::Type{T}, row, col)
+    @inbounds dest.values[row] = true
+end
+
+function handle_value_label!(val_labels::Cstring, value::DataReadValue, label::Cstring, ds_ptr::Ptr{DataStream})
     return convert(Cint, 0)
 end
 
 function read_data_file(filename::ASCIIString, filetype::Type)
-    # initialize ctx
-    ctx = DataReadCtx()
+    # initialize ds
+    ds = DataStream(Schema(DataType[]))
     # initialize parser
     parser = Parser()
     # parse
-    parse_data_file!(ctx, parser, filename, filetype)
-    # return dataframe
-    return DataFrame(ctx)
+    parse_data_file!(ds, parser, filename, filetype)
+    # return dataframe instead of DataStream
+    return DataFrame(convert(Vector{Any},ds.data),Symbol[symbol(x) for x in ds.schema.header])
 end
 
 for f in (:dta, :sav, :por, :sas7bdat) 
@@ -193,10 +174,10 @@ end
 
 function Parser()
     parser = ccall( (:readstat_parser_init, "libreadstat"), Ptr{Void}, ())
-    const info_fxn = cfunction(handle_info!, Cint, (Cint, Cint, Ptr{DataReadCtx}))
-    const var_fxn = cfunction(handle_variable!, Cint, (Cint, Ptr{DataReadVariable}, Cstring,  Ptr{DataReadCtx}))
-    const val_fxn = cfunction(handle_value!, Cint, (Cint, Cint, DataReadValue, Ptr{DataReadCtx}))
-    const label_fxn = cfunction(handle_value_label!, Cint, (Cstring, DataReadValue, Cstring, Ptr{DataReadCtx}))
+    const info_fxn = cfunction(handle_info!, Cint, (Cint, Cint, Ptr{DataStream}))
+    const var_fxn = cfunction(handle_variable!, Cint, (Cint, Ptr{DataReadVariable}, Cstring,  Ptr{DataStream}))
+    const val_fxn = cfunction(handle_value!, Cint, (Cint, Cint, DataReadValue, Ptr{DataStream}))
+    const label_fxn = cfunction(handle_value_label!, Cint, (Cstring, DataReadValue, Cstring, Ptr{DataStream}))
     ccall( (:readstat_set_info_handler, "libreadstat"), Int, (Ptr{Void}, Ptr{Void}), parser, info_fxn)
     ccall( (:readstat_set_variable_handler, "libreadstat"), Int, (Ptr{Void}, Ptr{Void}), parser, var_fxn)
     ccall( (:readstat_set_value_handler, "libreadstat"), Int, (Ptr{Void}, Ptr{Void}), parser, val_fxn)
@@ -208,11 +189,11 @@ for f in (:dta, :sav, :por, :sas7bdat)
     valtype = Val{f}
     # call respective parser
     @eval begin
-        function parse_data_file!(ctx::DataReadCtx, parser, 
+        function parse_data_file!(ds::DataStream, parser, 
             filename::ASCIIString, filetype::Type{$valtype})
             retval = ccall(($(string(:readstat_parse_, f)), "libreadstat"), 
-                        Int, (Ptr{Void}, Ptr{UInt8}, Ptr{DataReadCtx}),
-                        parser, bytestring(filename), pointer_from_objref(ctx))
+                        Int, (Ptr{Void}, Ptr{UInt8}, Any),
+                        parser, bytestring(filename), ds)
             ccall( (:readstat_parser_free, "libreadstat"), Void, (Ptr{Void},), parser)
             retval == 0 ||  error("Error parsing $filename: $retval")
         end
