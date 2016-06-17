f542bf573c5aa0c36d207c2e975aee899f6e8300
David Anthoff <anthoff@berkeley.edu>
2016-06-17 14:48:47 -0700

    Add source build to builds.jl for Unix

--- deps/build.jl
+++ deps/build.jl
@@ -4,6 +4,22 @@ using BinDeps
 
 libreadstat = library_dependency("libreadstat", aliases=["libreadstat-0"])
 
+provides(Sources, URI("http://github.com/WizardMac/ReadStat/archive/master.tar.gz"),
+    libreadstat, os=:Unix, unpacked_dir="ReadStat-master")
+
+srcdir = joinpath(BinDeps.depsdir(libreadstat), "src", "ReadStat-master")
+
+provides(SimpleBuild,
+    (@build_steps begin
+        GetSources(libreadstat)
+        @build_steps begin
+            ChangeDirectory(srcdir)
+            `./autogen.sh`
+            `./configure`
+            `make`
+        end
+    end), libreadstat, os=:Unix)
+
 @windows_only begin
     using WinRPM
     push!(WinRPM.sources, "http://download.opensuse.org/repositories/home:/davidanthoff/openSUSE_13.2")
