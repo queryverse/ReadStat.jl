a01d62f13fa2632a632eed941e025b38012cd5c8
David Anthoff <anthoff@berkeley.edu>
2019-02-23 12:55:43 -0800

    Add docs template

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Documenter, ReadStat
+
+makedocs(
+	modules = [ReadStat],
+	sitename = "ReadStat.jl",
+	analytics="UA-132838790-1",
+	pages = [
+        "Introduction" => "index.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/queryverse/ReadStat.jl.git"
+)
