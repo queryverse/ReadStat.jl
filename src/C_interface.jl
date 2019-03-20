1bbaef1f2ab8d7608a461dfa53714087ff542736
Tony Lian <1040424979@qq.com>
2019-03-19 23:00:30 -0700

    Add libreadstat into readstat_begin_row and readstat_end_row, pass pointer into readstat_begin_writing

--- src/C_interface.jl
+++ src/C_interface.jl
@@ -72,27 +72,27 @@ function readstat_parse(filename::String, type::Val{:sas7bdat}, parser::Ptr{Noth
 end
 
 function readstat_begin_row(writer)
-    return ccall((:readstat_begin_row), Int, (Ptr{Nothing},), writer)
+    return ccall((:readstat_begin_row, libreadstat), Int, (Ptr{Nothing},), writer)
 end
 
 function readstat_end_row(writer)
-    return ccall((:readstat_end_row), Int, (Ptr{Nothing},), writer)
+    return ccall((:readstat_end_row, libreadstat), Int, (Ptr{Nothing},), writer)
 end
 
 function readstat_begin_writing(writer, filetype::Val{:dta}, io, row_count)
-    return ccall((:readstat_begin_writing_dta, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, io, Cint(row_count))
+    return ccall((:readstat_begin_writing_dta, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, pointer_from_objref(io), Cint(row_count))
 end
 
 function readstat_begin_writing(writer, filetype::Val{:sav}, io, row_count)
-    return ccall((:readstat_begin_writing_sav, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, io, Cint(row_count))
+    return ccall((:readstat_begin_writing_sav, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, pointer_from_objref(io), Cint(row_count))
 end
 
 function readstat_begin_writing(writer, filetype::Val{:por}, io, row_count)
-    return ccall((:readstat_begin_writing_por, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, io, Cint(row_count))
+    return ccall((:readstat_begin_writing_por, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, pointer_from_objref(io), Cint(row_count))
 end
 
 function readstat_begin_writing(writer, filetype::Val{:sas7bdat}, io, row_count)
-    return ccall((:readstat_begin_writing_sas7bdat, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, io, Cint(row_count))
+    return ccall((:readstat_begin_writing_sas7bdat, libreadstat), Int, (Ptr{Nothing}, Ptr{Nothing}, Cint), writer, pointer_from_objref(io), Cint(row_count))
 end
 
 function readstat_insert_double_value(writer, variable, item)
